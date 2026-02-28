8dae13efa2ad08e820024373013a1c01b676a050
HeitorOrtega <rm557825@fiap.com.br>
2026-02-28 01:04:04 -0300

    colocando id

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ import os
 CANAIS_FECHAMENTOS = {
     "T1": int(os.getenv("CANAL_T1", "1333437341410529334")),  
     "T2": int(os.getenv("CANAL_T2", "1470909878460616714")), 
+    "T3": int(os.getenv("CANAL_T3", "1477142896334475405")), 
 }
